4e87f43a28972217518775ce0e685c915ab43f1a
timccheah <56974539+timccheah@users.noreply.github.com>
2020-03-10 10:00:28 -0500

    Update SG90_Servo_Motor.ino

--- VehicleAccessControlSystem/MFRC522_Reader/SG90_Servo_Motor.ino
+++ VehicleAccessControlSystem/MFRC522_Reader/SG90_Servo_Motor.ino
@@ -9,19 +9,24 @@ void servoSetup() {
 }
 
 void servoLoop() {
-   // scan from 0 to 180 degrees
-  for(angle = 10; angle < 180; angle++)  
+   // scan from 0 to 90 degrees
+  for(angle = 0; angle < 90; angle++)  
   {                                  
     servo.write(angle);               
     delay(15);                   
   } 
-  // now scan back from 180 to 0 degrees
-  for(angle = 180; angle > 10; angle--)    
-  {                                
-    servo.write(angle);           
-    delay(15);       
+  // now scan back from 90 to 0 degrees
+  while (check(distance) == true) {
+      HR_SR04Loop();
   } 
-
+  if (check(distance) == false) {
+    delay(1000);       
+    for(angle = 90; angle > 0; angle--)    
+    {                                
+      servo.write(angle);           
+      delay(15);       
+    } 
+  }
 }
 
 /*
